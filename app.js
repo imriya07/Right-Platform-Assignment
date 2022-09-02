8498fac36802e3338924cedf5bb337297fd1986e
apple <apple@apples-MacBook-Air.local>
2022-09-02 20:28:18 +0530

    modify code regarding password validation

--- app.js
+++ app.js
@@ -21,9 +21,22 @@ const togglePassword = document.querySelector('#togglePassword');
 
   togglePassword.addEventListener('click', function (e) {
     // toggle the type attribute
+    
     const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
     password.setAttribute('type', type);
     pass.setAttribute('type', type);
     // toggle the eye slash icon
     this.classList.toggle('fa-eye-slash');
+    
 });
+
+function add(value){
+  const password = document.querySelector('#pwd');
+  const pass = document.querySelector('#pwd2332');
+  if(password.value === pass.value){
+    alert("password  match");
+  }
+  else{
+    alert("password not match")
+  }
+}
